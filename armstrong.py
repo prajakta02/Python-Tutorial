d99e93435c25740bf25336036b12352404eb3db4
Prajakta Joshi <40360231+prajakta02@users.noreply.github.com>
2020-02-16 20:56:43 +0530

    Armstrong number code

---
+++ armstrong.py
@@ -0,0 +1,23 @@
+def armstrong_fun(a):
+	b = [int(x) for x in str(a)] 
+
+	sum=0
+
+	for k in b:
+
+		sum=sum+pow(k,len(b))
+
+	if(sum==a):
+		return 1
+	else:
+		return 0		
+
+print("Enter number you want to find armstrong number for: ")
+a=int(input())
+#print(" Your number is: ")
+result=armstrong_fun(a)
+if(result==1):
+	print("Armstrong number")
+else:
+	print("Not an armstrong number")
+	
